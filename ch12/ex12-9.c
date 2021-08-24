ed1f717a8a331b86cabfdcc5319e2120abffeed6
POL6463 <kevinkim0617kim@gmail.com>
2021-08-24 23:09:57 +0900

    8/24

---
+++ ch12/ex12-9.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    char str[20] = "mango tree";
+
+    strncpy(str, "apple-pie", 5);
+    str[5] = '\0';
+    printf("%s\n", str);
+
+    return 0;
+}
