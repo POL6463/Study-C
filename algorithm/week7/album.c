d63e318f6b69f0de8bc22db42be5d73867e9993a
POL6463 <kevinkim0617kim@gmail.com>
2021-10-18 20:18:22 +0900

    as

---
+++ algorithm/week7/album.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+struct Album
+{
+    char genre[50];
+    int play;
+};
+
+
+int main(void){
+    struct Album album[10005];
+    int num, i;
+
+    scanf("%d", &num);
+
+    for(i = 0; i < num; i++){
+        scanf("%s%d", album[i].genre, &album[i].play);
+    }
+
+    printf("%s, %d", album[0].genre, album[0].play);
+
+    return 0;
+}
