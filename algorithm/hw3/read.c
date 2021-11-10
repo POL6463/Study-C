50e59749361f0e2d6831c5024fc5b52febc31a7c
POL6463 <kevinkim0617kim@gmail.com>
2021-11-10 12:09:48 +0900

    함수형식으로 바꾸기

--- algorithm/hw3/read.c
+++ algorithm/hw3/read.c
@@ -1,15 +1,29 @@
 #include <stdio.h>
+#include <string.h>
 
 int arr[1000][1000] = {0};
+void readFileName(int *, int *, char *);
 
 int main(void){
     FILE *fp;
     char name[100]; //파일 이름 저장하는 배열
+    char compName[100] = "map-input-";
     int row, column;
 
     printf("파일 이름을 입력하세요: ");
     scanf("%s", name); //파일 이름 입력
-    printf("%s\n", name);
+
+    if (strncmp(compName, name, 10) == 0){  //파일 이름 앞부분 비교
+        printf("%s\n", name);
+    }
+    else{
+        printf("Error: Unable to open file %s", name); //앞부분 형식과 다를 시 프로그램 종료
+        return 0;
+    }
 
     return 0;
+}
+
+void readFileName(int *row, int *column, char *arr){
+    
 }
