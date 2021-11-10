d1595456e2afb33d9aae81be0ed5315b50506a9b
POL6463 <kevinkim0617kim@gmail.com>
2021-11-11 00:19:36 +0900

    파일에서 정수 받아오고 에러처리 완료

--- algorithm/hw3/read.c
+++ algorithm/hw3/read.c
@@ -5,12 +5,14 @@
 
 int arr[1000][1000] = {0};
 void readFileName(int *, int *, char *);
+void findMaxMin(int *, int *);
 
 int main(void){
     FILE *fp;
     char name[100]; //파일 이름 저장하는 배열
     int row = 0, column = 0;
     int i, j;
+    int max, min;
 
     readFileName(&row, &column, name); //파일 이름을 읽고 행, 열 읽는 함수
 
@@ -21,18 +23,22 @@ int main(void){
         exit(1);
     }
 
-    for(i = 0; i < column; i++){
+    for(i = 0; i < column; i++){ //파일의 데이터를 순차적으로 이중배열에 입력
         for(j = 0; j < row; j++){
-            if(EOF == fscanf(fp, "%d", &arr[i][j])){
-                printf("Error : End of file reached prior to getting all the required data\n");
+            if(feof(fp)){
+                printf("Error : End of file reached prior to getting all the required data\n"); //데이터의 수가 부족하면 에러 반환 후 종료
+                exit(1);
+            }
+            if(fscanf(fp, "%d", &arr[i][j]) == 0){ //정수형이 아니면 오류 반환후 종료
+                printf("Error : Read a non-integer value");
                 exit(1);
             }
         }
     }
 
-    if(EOF != fgetc(fp)){
-        printf("Error : Too many data points\n");
-        exit(1);
+    if(EOF != fgetc(fp)){ //아직 파일에 데이터가 남아있는지 확인 후 남아있으면 에러 발생
+            printf("Error : Too many data points\n");
+            exit(1);
     }
 
     
@@ -43,6 +49,8 @@ int main(void){
         }
         printf("\n");
     }
+
+    findMaxMin(&max, &min);
     
 
 
@@ -100,4 +108,8 @@ void readFileName(int *row, int *column, char *arr){
     for(i = secondNumCounter -1; i >= 0; i--){
         *column += secondNum[temp++] * pow(10, i);
     } //secondNum에 있는 숫자를 column에 저장
+}
+
+void findMaxMin(int *max, int *min){
+    
 }
