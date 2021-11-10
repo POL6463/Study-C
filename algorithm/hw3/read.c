873c78457c231e2b02ef101b5767ce83f7cdd18f
POL6463 <kevinkim0617kim@gmail.com>
2021-11-10 14:34:44 +0900

    첫번째 숫자 읽고 반환 완료

--- algorithm/hw3/read.c
+++ algorithm/hw3/read.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <math.h>
 
 int arr[1000][1000] = {0};
 void readFileName(int *, int *, char *);
@@ -7,23 +9,54 @@ void readFileName(int *, int *, char *);
 int main(void){
     FILE *fp;
     char name[100]; //파일 이름 저장하는 배열
-    char compName[100] = "map-input-";
-    int row, column;
+    int row = 0, column = 0;
 
-    printf("파일 이름을 입력하세요: ");
-    scanf("%s", name); //파일 이름 입력
+    readFileName(&row, &column, name); //파일 이름을 읽고 행, 열 읽는 함수
 
-    if (strncmp(compName, name, 10) == 0){  //파일 이름 앞부분 비교
-        printf("%s\n", name);
-    }
-    else{
-        printf("Error: Unable to open file %s", name); //앞부분 형식과 다를 시 프로그램 종료
-        return 0;
-    }
+
+
+    printf("%d\n", row);
 
     return 0;
 }
 
 void readFileName(int *row, int *column, char *arr){
-    
+    int i, temp;
+    char compName[100] = "map-input-";
+    int firstNum[100] = {0};
+    int firstNumCounter = 0;
+    int secondNum[100] = {0};
+    int secondNumCounter = 0;
+
+    printf("파일 이름을 입력하세요: ");
+    scanf("%s", arr);
+
+    if (strncmp(compName, arr, 10) == 0){  //파일 이름 앞부분 비교
+        printf("%s\n", arr);
+    }
+    else{
+        printf("Error: Unable to open file %s", arr); //앞부분 형식과 다를 시 프로그램 종료
+        exit(1);
+    }
+
+    for(int i = 0; arr[10 + i] != '-'; i++){
+        printf("%c", arr[10 + i]);
+        firstNum[i] = arr[10 + i] - '0';
+        firstNumCounter++;
+    }
+    printf("\n");
+
+    temp = 0;
+    for(i = firstNumCounter -1; i >= 0; i--){
+        *row += firstNum[temp++] * pow(10, i);
+    }
+
+    printf("\n");
+    for(int i = 0; arr[i] != 0 ;i++){
+        printf("%c", arr[i]);
+    }
+    printf("\n");
+
+
+
 }
