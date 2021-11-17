3ab6cc55bb177dd0e2f0137d6b4cb14ee5e09e2d
POL6463 <kevinkim0617kim@gmail.com>
2021-11-17 19:47:11 +0900

    FIFO 주석처리

--- architecture/test.c
+++ architecture/test.c
@@ -4,9 +4,9 @@
 typedef struct node_type{
     int data;
     struct node_type *next;
-}node;
+}node; //연결리스트로 쓸 구조체 선언
 
-typedef node *list;
+typedef node *list; //구조체의 주소를 담는 변수 list선언
 
 int searchList(list, int);
 list insert(list, list, int, int);
@@ -14,49 +14,48 @@ void showList(list);
 
 
 int main(void){
-    FILE *fp;
-    list head = NULL, tail = NULL;
-    int dat, count = 0, hit = 0;
-    int nums;
+    FILE *fp; //파일 주소 변수
+    list head = NULL, tail = NULL; //앞뒤 구분을 위해 head, tail선언
+    int dat, count = 0, hit = 0;//dat = 데이터값, count = 총 몇개의 데이터가 들어왔는지, hit = hit하면 증가
+    int nums;//현재 연결리스트가 몇개 연결되어있는지
 
-    fp = fopen("input.txt", "r");
+    fp = fopen("input.txt", "r"); //파일오픈
 
     while(1){
 
-        fscanf(fp, "%d", &dat);
-        if(dat == 99){
+        fscanf(fp, "%d", &dat); //파일에서 하나 꺼냄
+        if(dat == 99){// 99나오면 종료
             break;
         }
 
-        nums = searchList(head, dat);
+        nums = searchList(head, dat); //hit인지 아닌지 search하는 함수, 리스트에 몇개들어있는지 반환
         
-
         if(nums == 5){
-            hit++;
+            hit++;// num이 5이면 hit이기에 힛 증가
         }
-        else if(nums == 0){
+        else if(nums == 0){//첫번째로 들어왔으면 공간 만들어주고 헤드로 지정
             head = (list)malloc(sizeof(node));
             head->data = dat;
             head->next = NULL;
             tail = head;
         }
         else{
-            tail = insert(head, tail, dat, nums);
+            tail = insert(head, tail, dat, nums);//나머지 들어왔을경우 insert 함수 호출
             if(nums == 4){
-                head = head->next;
+                head = head->next;//만약 4개있는상태에서 hit이아닌데이터가 들어왔을때 제일 앞의 head 하나 줄임
             }
         }
 
-        showList(head);
-        printf("dat : %d\n", dat);
+        //showList(head); //마지막으로 메모리에 있는 숫자들 출력
+        //printf("dat : %d\n", dat); 
 
 
         count++;
     }
 
-    showList(head);
+    showList(head); //마지막으로 메모리에 있는 숫자들 출력
 
-    printf("적중률 = %d/%d\n", hit, count);
+    printf("적중률 = %d/%d\n", hit, count); //적중률 출력
    
     return 0;
 
