6e9e1e75880e9fedffba0f5eeecc6dceabff0b91
POL6463 <kevinkim0617kim@gmail.com>
2021-11-19 12:39:40 +0900

    animal.c 완료

--- ch14/ex14-3.c
+++ ch14/ex14-3.c
@@ -1,21 +1,28 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
-    char animal[5][20];
+    char animal[5][80];
     int i;
     int count;
+    int used = 0;
 
     count = sizeof(animal)/ sizeof(animal[0]);
     for (i = 0; i < count; i++)
     {
-        scanf("%s", animal[i]);
+        gets(animal[i]);
+        used += (strlen(animal[i]) + 1);
     }
 
     for (i = 0; i < count; i++)
     {
-        printf("%s ", animal[i]);
+        printf("%s\n", animal[i]);
     }
+
+    printf("전체 바이트: %lu, 사용된 바이트: %d, 사용 비율: %.2lf%%\n", sizeof(animal), used, (double)used/400 * 100);
+
+
 
     return 0;
 }
