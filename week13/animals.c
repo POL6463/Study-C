a4e24d0938ada85bb1a914bf71e23152639df13f
POL6463 <kevinkim0617kim@gmail.com>
2021-11-26 18:53:13 +0900

    주석입력

--- week13/animals.c
+++ week13/animals.c
@@ -3,27 +3,27 @@
 #include <string.h>
 
 int main(void){
-    char * animals[20] = {0};
-    char temp[80];
+    char * animals[20] = {0}; //최대 20개의 주소를 저장할 배열
+    char temp[80]; //입력받은값 임시로 저장할 변수 선언
     int i;
-    char enter[5] = "\0";
+    char enter[5] = "\0"; //엔터만 쳤을경우를 알기위해 선언
 
     for(i = 0; i < 20; i++){
         printf("input: ");
-        gets(temp);
+        gets(temp);//입력받은값 임시로 저장
         
-        animals[i] = (char *)malloc(strlen(temp) + 1);
-        strcpy(animals[i], temp);
-        if(strcmp(temp, enter)==0){
+        animals[i] = (char *)malloc(strlen(temp) + 1); //저장되어있는 문장의 크기 + 1만큼 공간 할당
+        strcpy(animals[i], temp);//할당받은 공간안에 복사
+        if(strcmp(temp, enter)==0){ //만악 들어온값이 엔터뿐이면 중단
             break;
         }
     }
 
     for(i = 0; i < 20; i++){
-        if(strcmp(animals[i], enter)==0){
+        if(strcmp(animals[i], enter)==0){ //만약 엔터만있는부분오면 중단
             break;
         }
-        printf("%s\n", animals[i]);
+        printf("%s\n", animals[i]); //순차적으로 출력
     }
 
     return 0;
--- week13/student.c
+++ week13/student.c
@@ -3,26 +3,26 @@
 #include <string.h>
 
 int main(void){
-    struct student{
+    struct student{ //이름과 성적 입력받을 구조체 선언
         char name[20];
         double grade;
     };
 
-    struct student list[5] = {0};
+    struct student list[5] = {0};//구조체 배열 선언
     int i;
     
     
 
-    for(i = 0; i < 3; i++){
+    for(i = 0; i < 3; i++){//3명의 학생을 받음
         printf("name: ");
-        gets(list[i].name);
+        gets(list[i].name); //이름입력
         printf("grade: ");
-        scanf("%lf", &list[i].grade);
-        fflush(stdin);
+        scanf("%lf", &list[i].grade);//성적입력
+        fflush(stdin); //성적입력시 엔터가 남아있어서 버퍼 비우기
     }
 
     for(i = 0; i < 3; i++){
-        printf("%s, %.1lf\n", list[i].name, list[i].grade);
+        printf("%s, %.1lf\n", list[i].name, list[i].grade);//최종 출력
 
     }
 
