a5aeeef3aba4e1d91b2b9a3247d0ce4dcf99bafa
POL6463 <kevinkim0617kim@gmail.com>
2021-11-27 22:56:46 +0900

    asdf

--- week13/student.c
+++ week13/student.c
@@ -4,14 +4,12 @@
 
 int main(void){
     struct student{ //이름과 성적 입력받을 구조체 선언
-        char name[20];
+        char name[80];
         double grade;
     };
 
     struct student list[5] = {0};//구조체 배열 선언
     int i;
-    
-    
 
     for(i = 0; i < 3; i++){//3명의 학생을 받음
         printf("name: ");
